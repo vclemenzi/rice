bf6b06e0f2f0dd00b978f48f3a7a9401d07afa53
vclemenzi <vclemenzi@users.noreply.github.com>
2023-08-26 08:28:24 +0200

    add aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
+alias pac="sudo pacman"
+alias ttc="cd $HOME/.config"
+
 function fish_prompt -d "Write out the prompt"
     printf '%s%s%s%s%s %s%s \n%s ' "["(set_color blue)$USER(set_color normal)(set_color red)"@"(set_color normal)(set_color cyan)$hostname(set_color normal)"]" (set_color yellow) " ➜" (set_color normal) (set_color green) (prompt_pwd) (set_color normal) (set_color red)"\$"(set_color normal) 
 end
