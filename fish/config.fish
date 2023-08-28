cd0f9570ccbd83afad0968f070d115949716212c
vclemenzi <vclemenzi@users.noreply.github.com>
2023-08-28 08:38:02 +0200

    minor changes

--- fish/config.fish
+++ fish/config.fish
@@ -12,6 +12,7 @@ alias ttci="cd $HOME/.config/i3"
 alias ttcn="cd $HOME/.config/nvim"
 alias ttcp="cd $HOME/.config/polybar"
 alias ttcf="cd $HOME/.config/fish"
+alias ttct="cd $HOME/.config/tmux"
 
 function fish_prompt -d "Write out the prompt"
     printf '%s%s%s%s%s %s%s \n%s ' "["(set_color blue)$USER(set_color normal)(set_color red)"@"(set_color normal)(set_color cyan)$hostname(set_color normal)"]" (set_color yellow) " ➜" (set_color normal) (set_color green) (prompt_pwd) (set_color normal) (set_color red)"\$"(set_color normal) 
