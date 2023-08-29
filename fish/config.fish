f87fab520f0ea8253504390fe2a7c758a91de850
vclemenzi <vclemenzi@users.noreply.github.com>
2023-08-29 15:56:26 +0200

    minor changes

--- fish/config.fish
+++ fish/config.fish
@@ -17,5 +17,5 @@ alias ttct="cd $HOME/.config/tmux"
 set fish_greeting
 
 function fish_prompt -d "Write out the prompt"
-    printf '%s%s%s%s%s %s%s \n%s ' "["(set_color blue)$USER(set_color normal)(set_color red)"@"(set_color normal)(set_color cyan)$hostname(set_color normal)"]" (set_color yellow) " ➜" (set_color normal) (set_color green) (prompt_pwd) (set_color normal) (set_color red)"\$"(set_color normal) 
+    printf '%s%s%s%s%s %s%s \n%s ' "["(set_color blue)$USER(set_color normal)(set_color red)"@"(set_color normal)(set_color cyan)$hostname(set_color normal)"]" (set_color yellow) " ➜ " (set_color normal) (set_color green) (prompt_pwd) (set_color normal) (set_color red)"\$"(set_color normal) 
 end
