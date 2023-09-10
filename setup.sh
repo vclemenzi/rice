46dfe38535d4be9a8be6064e91993943add9fc03
vclemenzi <vclemenzi@users.noreply.github.com>
2023-09-10 18:34:03 +0200

    feat(setup)!: new colors and style

--- setup.sh
+++ setup.sh
@@ -1,33 +1,76 @@
 #!/bin/bash
 
-echo "Installation of build dependencies in progress..."
-sudo pacman -S --needed git base-devel
-sudo pacman -S git
+print_dots() {
+  for i in {3..1}; do
+    echo -n "."
+    sleep 0.3
+  done
+}
 
-echo "Directory deletion..."
+echo " ______________________________________________"
+echo "/ Welcome to vclemenzi's rice setup!          \\"
+echo -e "| Remember this action is \033[0;31mirreversible\033[0m!        |"
+echo "| Any problems can be reported at:             |"
+echo -e "\\  \033[0;36mhttps://gh.vclemenzi.dev/rice\033[0m              /"
+echo " ---------------------------------------------"
+echo "        \\  ^__^"
+echo -e "         \\ (\033[0;33moo\033[0m)\________"
+echo -e "           (\033[0;31m__\033[0m)\        )\\/\\"
+echo "                ||------w|"
+echo "                ||      ||"
 
-rm -rf ~/.config/alacritty
-rm -rf ~/.config/fish
-rm -rf ~/.config/i3
-rm -rf ~/.config/neofetch
-rm -rf ~/.config/nvim
-rm -rf ~/.config/picom
-rm -rf ~/.config/polybar
-rm -rf ~/.config/rofi
-rm -rf ~/.config/tmux
-rm -rf ~/.config/mpv
+echo -n "The installation will start in: "
 
-echo "Installation of dependencies in progress... (this can take a while)"
-sudo pacman -S picom feh i3 fish neofetch i3lock polybar rofi alacritty tmux neovim xclip mpv flameshot
+echo -n -e "\033[0;32m5\033[0m"
+print_dots
 
-echo "Font installation in progress..."
-git clone https://aur.archlinux.org/nerd-fonts-meta.git ~/.cache/vclemenzi-rice-fonts-setup-temp
-sudo pacman -S fontconfig xorg-mkfontscale xorg-mkfontdir fontforge
-cd ~/.cache/vclemenzi-rice-fonts-setup-temp && makepkg -si
+echo -n -e "\033[0;33m4\033[0m"
+print_dots
+
+echo -n -e "\033[0;33m3\033[0m"
+print_dots
+
+echo -n -e "\033[0;33m2\033[0m"
+print_dots
+
+echo -n -e "\033[0;31m1\033[0m"
+print_dots
+
+echo -n -e "\033[0;31m0\033[0m"
+print_dots
+
+echo -n -e " \033[0;33mStarting...\033[0m\n"
+sleep 1
+
+if ! sudo echo -n -e ""; then
+  exit 1
+fi
+
+# Step 1: Installation of prerequisites
+echo -ne '[\033[0;90m---------------------------\033[0m] (0%)\r'
+sudo pacman -S --noconfirm --needed git base-devel &> /dev/null
+sudo pacman -S --noconfirm git &> /dev/null
+
+# Step 2: Directory deletion
+echo -ne '[\033[0;33m########\033[0m\033[0;90m-------------------\033[0m] (20%)\r'
+rm -rf ~/.config/alacritty ~/.config/fish ~/.config/i3 ~/.config/neofetch ~/.config/nvim ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/tmux ~/.config/mpv &> /dev/null
+
+# Step 3: Installation of dependencies
+echo -ne '[\033[0;33m############\033[0m\033[0;90m---------------\033[0m] (40%)\r'
+sudo pacman -S --noconfirm picom feh i3 fish neofetch i3lock polybar rofi alacritty tmux neovim xclip mpv flameshot &> /dev/null
+
+# Step 4: Font installation
+echo -ne '[\033[0;33m##############\033[0m\033[0;90m-------------\033[0m] (50%)\r'
+git clone https://aur.archlinux.org/nerd-fonts-meta.git ~/.cache/vclemenzi-rice-fonts-setup-temp &> /dev/null
+echo -ne '[\033[0;33m#################\033[0m\033[0;90m----------\033[0m] (60%)\r'
+sudo pacman -S --noconfirm fontconfig xorg-mkfontscale xorg-mkfontdir fontforge &> /dev/null
+echo -ne '[\033[0;33m####################\033[0m\033[0;90m-------\033[0m] (70%)\r'
+cd ~/.cache/vclemenzi-rice-fonts-setup-temp && makepkg -si --noconfirm &> /dev/null
 cd ..
 
-echo "Downloading the repo in progress..."
-git clone https://github.com/vclemenzi/rice ~/.cache/vclemenzi-rice-temp/
+# Step 5: Downloading the repo
+echo -ne '[\033[0;33m######################\033[0m\033[0;90m-----\033[0m] (80%)\r'
+git clone https://github.com/vclemenzi/rice ~/.cache/vclemenzi-rice-temp/ &> /dev/null
 
 cp -r ~/.cache/vclemenzi-rice-temp/alacritty/ ~/.config/
 cp -r ~/.cache/vclemenzi-rice-temp/fish/ ~/.config/
@@ -40,4 +83,12 @@ cp -r ~/.cache/vclemenzi-rice-temp/rofi/ ~/.config/
 cp -r ~/.cache/vclemenzi-rice-temp/tmux/ ~/.config/
 cp -r ~/.cache/vclemenzi-rice-temp/mpv/ ~/.config/
 
-rm -rf ~/.cache/vclemenzi-rice-temp/
+echo -ne '[\033[0;33m########################\033[0m\033[0;90m---\033[0m] (90%)\r'
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &> /dev/null
+tmux source ~/.config/tmux/tmux.conf &> /dev/null
+
+rm -rf ~/.cache/vclemenzi-rice-temp/ &> /dev/null
+
+echo -ne '[\033[0;32m###########################\033[0m] (100%)\r'
+sleep 1
+echo -ne "\033[0;32mCongratulations! The rice has been successfully installed!\033[0m"
