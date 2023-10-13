5eb1617c0e25273e5a5da5192a7938d4e15c9e75
vclemenzi <vclemenzi@users.noreply.github.com>
2023-10-13 20:31:46 +0200

    feat: nvim: motions

--- nvim/lua/core/remap.lua
+++ nvim/lua/core/remap.lua
@@ -1,1 +1,7 @@
-vim.api.nvim_set_keymap('n', '<leader>tn', [[<cmd>tabnew<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-c>', [[<cmd>tabnew<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-q>', [[<cmd>tabclose<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-a>', [[<cmd>tabprev<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-d>', [[<cmd>tabnext<CR>]], { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<C-s>', [[<cmd>split<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-w>', [[<cmd>vsplit<CR>]], { noremap = true, silent = true })
